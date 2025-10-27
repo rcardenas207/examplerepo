526ea05ea9d54f68df358dd4614a09170bb56f56
rcardenas207 <rcardenas5036@stu.neisd.net>
2025-10-27 13:39:41 -0500

    my first commit!

---
+++ ExampleProgram.py
@@ -0,0 +1,14 @@
+a = "String 1"
+b = "String 2"
+
+print("a =" + a + " and b= + b)
+
+x = "string 3"
+
+y = "string 4"
+
+print(f"x = {x} and y = {y}")
+
+
+
+print("c = {c} and d = {d}".format(c=1, d=2))
